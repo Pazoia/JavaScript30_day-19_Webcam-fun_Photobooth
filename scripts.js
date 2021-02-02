50ee0c5fc55804d14c4b4ac55371d45ab4692886
Paulo Azoia <pazoia.gomes@gmail.com>
2021-02-02 18:11:20 +0000

    Color toggles manipulating picture as they are moved

--- scripts.js
+++ scripts.js
@@ -26,7 +26,12 @@ function paintToCanvas() {
         // take the pixels out
         let pixels = ctx.getImageData(0, 0, width, height);
         // mess with them
-        pixels = redEffect(pixels);
+
+        // pixels = redEffect(pixels);
+        // pixels = rgbSplit(pixels);
+        // ctx.globalAlpha = 0.1;
+        pixels = greenScreen(pixels);
+
         // put them back
         ctx.putImageData(pixels, 0, 0);
 
@@ -52,6 +57,40 @@ function redEffect(pixels) {
         pixels.data[i + 0] = pixels.data[i + 0] + 100; // Red
         pixels.data[i + 1] = pixels.data[i + 1] - 50; // Green
         pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i - 150] = pixels.data[i + 0] + 100; // Red
+        pixels.data[i + 100] = pixels.data[i + 1] - 50; // Green
+        pixels.data[i - 150] = pixels.data[i + 2] * 0.5; // Blue
+    }
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+    });
+
+    for (i = 0; i < pixels.data.length; i += 4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+
+        if (red >= levels.rmin
+            && green >= levels.gmin
+            && blue >= levels.bmin
+            && red <= levels.rmax
+            && green <= levels.gmax
+            && blue <= levels.bmax) {
+                pixels.data[i + 3] = 0;
+            }
     }
     return pixels;
 }
