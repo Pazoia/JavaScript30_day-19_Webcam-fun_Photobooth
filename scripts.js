2f0a14b32af21e4deda9f8ec6aae45dea5944a7d
Paulo Azoia <pazoia.gomes@gmail.com>
2021-02-02 17:26:07 +0000

    Created and applied redEffect to video in canvas

--- scripts.js
+++ scripts.js
@@ -23,6 +23,13 @@ function paintToCanvas() {
 
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
+        // take the pixels out
+        let pixels = ctx.getImageData(0, 0, width, height);
+        // mess with them
+        pixels = redEffect(pixels);
+        // put them back
+        ctx.putImageData(pixels, 0, 0);
+
     }, 16);
 }
 
@@ -38,6 +45,15 @@ function takePhoto() {
     link.setAttribute('download', 'image');
     link.innerHTML = `<img src="${data}" alt="Handsome Man"/>`;
     strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 100; // Red
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; // Green
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+    }
+    return pixels;
 }
 
 getVideo();
