2a5e111bab4e6ab0913ce394ccb6cd199a50033b
Paulo Azoia <pazoia.gomes@gmail.com>
2021-02-02 16:51:21 +0000

    Take Photo button collecting a photo:
    when Take Photo is clicked a snap pict is collected
    when Take Photo is clecked a snap sound is played

--- scripts.js
+++ scripts.js
@@ -26,4 +26,20 @@ function paintToCanvas() {
     }, 16);
 }
 
+function takePhoto() {
+    // played the sound
+    snap.currentTime = 0;
+    snap.play();
+
+    // take the data out of the canvas
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'image');
+    link.innerHTML = `<img src="${data}" alt="Handsome Man"/>`;
+    strip.insertBefore(link, strip.firstChild);
+}
+
 getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
